11636347c6e4d8b8a83b348b54fd8eb90708106f
manish Reddy <manishreddy255@gmail.com>
2020-08-21 11:50:38 +0530

    first things

---
+++ script.js
@@ -0,0 +1,45 @@
+// console.log('hello world!!!!!!!!');
+
+
+// now we get into variables and data types
+
+
+// var firstName = "john";
+// console.log(firstName)
+
+// var lastName = 'schmidt';
+// var age = 34;
+// console.log(age)
+
+// var isTrue = true;
+// console.log(isTrue)
+
+// // when there is nothing coming from the value we 
+// // get a value of undefined
+// var job;
+// console.log(job)
+
+// job = 'manish'
+// console.log(job)
+
+//camel casing is used to define javascript variales
+
+
+/* variable mutation and type coercion
+*/
+
+var firstName = 'John';
+var age = 20;
+// so in type coercion the number is automatically converted to a string so that it
+// it can be displayed as a sting in the console 
+// in other languages like java we need to change number to string in order to change
+// print it to the page or something
+console.log(firstName + ' ' + age)
+
+var job, isMarried;
+job = 'teacher';
+isMarried = false
+
+console.log(`${firstName} is of ${age} and he is a ${job}. is he married ? ${isMarried}`)
+
+
